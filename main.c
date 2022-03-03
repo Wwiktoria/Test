a832ae3e73f2c62051ca41573342e00998fedd23
Wwiktoria1 <100830693+Wwiktoria@users.noreply.github.com>
2022-03-03 08:25:23 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define pi 3.14
+int main(void) {
+  int r = 3; /* jhfuhfueihfuehfuhueifheufhuefheiwfhuewhfuehfuefu*/
+  double pole = pi*r*r;
+  printf("Pole jest równe: %f\n",pole );
+  return 0;
+}/* zadanie funkcja skan f i przetestować użycie (wprowadzanie danych do programu) zrobić zadania, aktywacja konta*/
